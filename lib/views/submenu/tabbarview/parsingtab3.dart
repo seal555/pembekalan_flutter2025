107c0721fb6c0618d7583074e79224ec412e75d9
Eric Agustian <boku.no.crazy@gmail.com>
2025-01-17 11:08:33 +0700

    17/01/2025 siang

--- lib/views/submenu/tabbarview/parsingtab3.dart
+++ lib/views/submenu/tabbarview/parsingtab3.dart
@@ -1,5 +1,6 @@
 //stf shorcut
 import 'package:flutter/material.dart';
+import 'package:pembekalan_flutter/customs/custom_snackbar.dart';
 import 'package:pembekalan_flutter/models/list_users_model.dart';
 import 'package:pembekalan_flutter/viewmodels/list_users_viewmodel.dart';
 
@@ -13,6 +14,27 @@ class ParsingTab3 extends StatefulWidget {
 class _Parsingtab3State extends State<ParsingTab3>
     with AutomaticKeepAliveClientMixin {
   List<Data> dataUsers = List<Data>.empty();
+  int page = 1;
+  int maxPage = 1;
+
+  // untuk handling pergerakan scroll pada layar
+  ScrollController scrollController = new ScrollController();
+
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    scrollController.addListener(() {
+      if (scrollController.position.pixels ==
+          scrollController.position.maxScrollExtent) {
+        // ini artinya scroll sudah mentok bawah
+        loadMoreData();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +46,7 @@ class _Parsingtab3State extends State<ParsingTab3>
             TextButton(
               onPressed: () {
                 // get data
-                getListUsers(1);
+                getListUsers(page);
               },
               child: Text('Get List Users'),
               style: ButtonStyle(
@@ -33,63 +55,74 @@ class _Parsingtab3State extends State<ParsingTab3>
             ),
             Expanded(
                 child: ListView.builder(
-                    itemCount: dataUsers.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                        child: InkWell(
-                          onTap: () {
-                            // aksi tap
-                          },
-                          child: Card(
-                            color: index % 2 == 0
-                                ? Colors.white
-                                : Colors.amberAccent,
-                            margin: EdgeInsets.fromLTRB(5, 10, 5, 10),
-                            child: Column(
+              itemCount: dataUsers.length + 1,
+              itemBuilder: (BuildContext context, int index) {
+                if (index == dataUsers.length) {
+                  return loadingIndicator();
+                } else {
+                  return Container(
+                    child: InkWell(
+                      onTap: () {
+                        // aksi tap
+                        showInfoUser(dataUsers[index]);
+                      },
+                      child: Card(
+                        color:
+                            index % 2 == 0 ? Colors.white : Colors.amberAccent,
+                        margin: EdgeInsets.fromLTRB(5, 30, 5, 30),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Row(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    CircleAvatar(
-                                      radius: 40,
-                                      backgroundImage:
-                                          NetworkImage(dataUsers[index].avatar),
-                                    ),
-                                    Padding(
-                                      padding: EdgeInsets.all(5),
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            '${dataUsers[index].firstName} ${dataUsers[index].lastName}',
-                                            style: TextStyle(
-                                                fontSize: 16,
-                                                fontWeight: FontWeight.bold),
-                                          ),
-                                          Text(
-                                            '${dataUsers[index].email}',
-                                            style: TextStyle(
-                                                fontSize: 14,
-                                                fontWeight: FontWeight.normal),
-                                          )
-                                        ],
+                                CircleAvatar(
+                                  radius: 40,
+                                  backgroundImage:
+                                      NetworkImage(dataUsers[index].avatar),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.all(5),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        '${dataUsers[index].firstName} ${dataUsers[index].lastName}',
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold),
                                       ),
-                                    )
-                                  ],
+                                      Text(
+                                        '${dataUsers[index].email}',
+                                        style: TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.normal),
+                                      )
+                                    ],
+                                  ),
                                 )
                               ],
-                            ),
-                          ),
+                            )
+                          ],
                         ),
-                      );
-                    }))
+                      ),
+                    ),
+                  );
+                }
+              },
+              controller: scrollController,
+            ))
           ]),
     );
   }
 
   void getListUsers(int page) async {
+    setState(() {
+      // tampilkan loading
+      isLoading = true;
+    });
+
     // panggil API get list users dng parameter page
     // referensi:
     // https://www.dicoding.com/blog/tips-design-pattern-mvvm/
@@ -98,13 +131,52 @@ class _Parsingtab3State extends State<ParsingTab3>
 
     ListUsersViewmodel().getDataFromAPI(context, page).then((value) {
       setState(() {
+        // hilangkan loading
+        isLoading = false;
+
         if (value != null) {
           print('Debug : $value');
 
-          dataUsers = value.data;
+          // maximum page
+          maxPage = value.totalPages;
+
+          if (page == 1) {
+            // data awal kosong, isi data
+            dataUsers = value.data;
+          } else {
+            // sudah ada data sebelumnya, tambah data baru
+            dataUsers.addAll(value.data);
+          }
         }
       });
     });
+  }
+
+  void loadMoreData() {
+    print('Load more dipanggil $page');
+    if (dataUsers.isNotEmpty && page < maxPage) {
+      // ambil data selanjutnya
+      page += 1;
+      getListUsers(page);
+    } else {
+      // sudah maksimal, tidak ada baru
+    }
+  }
+
+  Widget loadingIndicator() {
+    // loading tipe load more
+    return Padding(
+      padding: EdgeInsets.all(5),
+      child: Center(
+        child: Opacity(
+            opacity: isLoading ? 1.0 : 0.0, child: CircularProgressIndicator()),
+      ),
+    );
+  }
+
+  void showInfoUser(Data info){
+    String message = '${info.firstName} ${info.lastName}';
+    ScaffoldMessenger.of(context).showSnackBar(customSnackbar(message));
   }
 
   @override
